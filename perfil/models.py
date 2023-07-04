e494cc1787c02adc16b3a81ddabab08e4a60093e
and <andersonvittor1801@gmail.com>
2023-07-04 20:31:19 -0300

    Segundo commit

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# Criando tabelas no banco de dados
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50)
+    essencial = models.BooleanField(default=False)
+    valor_planejamento = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.categoria
+
+class Conta(models.Model):
+    banco_choices = (
+        ('NU', 'Nubank'),
+        ('IN', 'Inter'),
+        ('BR', 'Bradesco'),
+        ('IT', 'ITAU')
+    )
+
+    tipo_choices = (
+        ('pf', 'Pessoa física'),
+        ('pj', 'Pessoa jurídica')
+    )
+    apelido = models.CharField(max_length=50)
+    banco = models.CharField(max_length=2, choices=banco_choices)
+    tipo = models.CharField(max_length=2, choices=tipo_choices)
+    valor = models.FloatField()
+    icone = models.ImageField(upload_to="icones")
+
+    def __str__(self):
+        return self.apelido
