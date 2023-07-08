b2c41a61878eceb6de5248dafd57e9dea79f36b7
and <andersonvittor1801@gmail.com>
2023-07-07 22:27:13 -0300

    Sétimo commit

--- planejamento/views.py
+++ planejamento/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from perfil.models import Categoria
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from .utils import calcula_total
 
 def definir_planejamento(request):
     categorias = Categoria.objects.all()
@@ -9,4 +11,12 @@ def definir_planejamento(request):
 
 @csrf_exempt
 def update_valor_categoria(request, id):
-    return HttpResponse(id)
+    novo_valor = json.load(request)['novo_valor']
+    categoria = Categoria.objects.get(id=id)
+    categoria.valor_planejamento = novo_valor
+    categoria.save()
+    return JsonResponse({'Status': 'Sucesso'})
+
+def ver_planejamento(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'ver_planejamento.html', {'categorias': categorias})
